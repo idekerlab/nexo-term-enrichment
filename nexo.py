12ed399039c687c68c4e32cb847875e10d71e922
Keiichiro Ono <keiono@gmail.com>
2013-07-24 16:36:05 -0700

    First working version for NeXO.  Need to support GO.

--- nexo.py
+++ nexo.py
@@ -5,90 +5,161 @@ from scipy import stats
 
 from flask import Flask
 from flask.ext import restful
+from flask.ext.restful import Resource, fields, reqparse
+
+import json
+
+# Constants
+GENES = 'genes'
+ALPHA = 'alpha'
+
+# p-value cutoff
+p_val_threshold = 0.01
+
+# At least this number of genes should be assigned
+gene_threshold = 2
 
 app = Flask(__name__)
 api = restful.Api(app)
 
+parser = reqparse.RequestParser()
+parser.add_argument(GENES, type=str)
+parser.add_argument(ALPHA, type=float)
+
+
 class TermMapping:
 
-  __gene2nexo_terms = {}
-  __nexo_term2genes = {}
+    _gene2nexo_terms = {}
+    _nexo_term2genes = {}
+    _num_nexo_genes = 0
 
-  def __init__(self):
-    print "Loading resource"
-    self.__gene2nexo_terms = self.createMap("data/gene2terms.txt")
-    self.__nexo_term2genes = self.createMap("data/term2genes.txt")
+    def __init__(self):
+        print "Loading resource"
+        self._gene2nexo_terms = self.createMap("data/gene2terms.txt")
+        self._nexo_term2genes = self.createMap("data/term2genes.txt")
+        # Count genes.  Root term has all genes.
+        all_genes = self._nexo_term2genes["joining_root"]
+        self._num_nexo_genes = len(all_genes)
+        print("NeXO Genes = " + str(self._num_nexo_genes))
 
-  def createMap(self, file_name):
-    mapping = {}
-    with open(file_name, "r") as f:
-      for line in f:
-        line = line.rstrip()
-        parts = line.split("\t")
-        geneID = parts[0]
-        terms = parts[1].split(",")
-        mapping[geneID] = terms
-    f.close()
-    return mapping
-
-
-  def getMapping(self):
-    return self.__gene2nexo_terms
-
-  def getTermMapping(self):
-    return self.__nexo_term2genes
+    def createMap(self, file_name):
+        mapping = {}
+        with open(file_name, "r") as f:
+            for line in f:
+                line = line.rstrip()
+                parts = line.split("\t")
+                geneID = parts[0]
+                terms = parts[1].split(",")
+                mapping[geneID] = terms
+        f.close()
+        return mapping
 
 
-class HypergeometricTester:
-  def __init__(self):
-    print("Tester Initialized")
+    def get_gene_mapping(self):
+        return self._gene2nexo_terms
 
-  def performTest(self,gene2terms,term2genes,genes_of_interest,total_num_genes):
-    sample_map = self.__calculateTermFrequency(genes_of_interest, gene2terms)
+    def get_term_mapping(self):
+        return self._nexo_term2genes
 
-    p_values = {}
-
-    n = len(genes_of_interest)
-
-    for term in sample_map:
-      # Calculate p-value
-      sampled = sample_map[term]
-      assigned_genes = term2genes[term]
-      k = len(sampled)
-      m = len(assigned_genes)
-      p = stats.hypergeom.pmf(k,total_num_genes,m,n)
-  
-
-  def __calculateTermFrequency(self, genes_of_interest, gene2terms):
-    sample_term_map = {}
-
-    for gene in genes_of_interest:
-      terms = gene2terms[gene]
-      for term in terms:
-        if term in sample_term_map:
-          assigned_genes = sample_term_map[term]
-          assigned_genes.add(gene)
-          sample_term_map[term] = assigned_genes
-        else:
-          assigned_genes = set([])
-          assigned_genes.add(gene)
-          sample_term_map[term] = assigned_genes
-    
-    return sample_term_map
+    def get_gene_count(self):
+        return self._num_nexo_genes
 
 
+class HypergeometricTest:
+
+    _mapper = None
+
+    def __init__(self):
+        self._mapper = TermMapping()
+
+    def performTest(self, genes_of_interest, cutoff):
+        if cutoff is None:
+            cutoff = p_val_threshold
+
+        total_num_genes = self._mapper.get_gene_count()
+        term2genes = self._mapper.get_term_mapping()
+        gene2terms = self._mapper.get_gene_mapping()
+
+        sample_map = self.__calculateTermFrequency(genes_of_interest, gene2terms)
+
+        n = len(genes_of_interest)
+
+        # Number of tests performed: will be used for correction.
+        num_tests = len(sample_map)
+
+        results = []
+
+        for term in sample_map:
+            # Calculate p-value
+            sampled = sample_map[term]
+            assigned_genes = term2genes[term]
+            k = len(sampled)
+            m = len(assigned_genes)
+
+            p = stats.hypergeom.pmf(k,total_num_genes,m,n)
+            p_corrected = p * num_tests
+
+            if p_corrected < cutoff and k >= gene_threshold:
+                result = {}
+                result['id'] = term
+                result['p-value'] = p_corrected
+                result['background'] = m
+                result['genes'] = list(sampled)
+                results.append(result)
+
+        return {'results':results, 'total_genes':total_num_genes}
 
 
-class TermEnrichement(restful.Resource):
-  def get(self):
-    p = stats.hypergeom.pmf(3,6381,27,5)
-    return {'nexo': 'v1', 'P-val': p}
+    def __calculateTermFrequency(self, genes_of_interest, gene2terms):
+        sample_term_map = {}
 
-api.add_resource(TermEnrichement, '/')
+        for gene in genes_of_interest:
+            terms = []
+            if gene in gene2terms:
+                terms = gene2terms[gene]
+            else:
+                continue
+            
+            for term in terms:
+                if term in sample_term_map:
+                    assigned_genes = sample_term_map[term]
+                    assigned_genes.add(gene)
+                    sample_term_map[term] = assigned_genes
+                else:
+                    assigned_genes = set([])
+                    assigned_genes.add(gene)
+                    sample_term_map[term] = assigned_genes
+        return sample_term_map
 
-# Main 
+
+class TermEnrichment(restful.Resource):
+    def get(self):
+        return {'results':'use POST to get results.'}
+
+    def post(self):
+        results = []
+        args = parser.parse_args()
+
+        # Gene names are case INSENSITIVE!
+        genes = args[GENES]
+        cutoff = args[ALPHA]
+
+        gene_list = genes.split()
+        genes_upper = []
+        for gene in gene_list:
+            genes_upper.append(gene.upper())
+
+        print(genes_upper)
+        print(cutoff)
+        return tester.performTest(genes_upper, cutoff)
+
+api.add_resource(TermEnrichment, '/enrich')
+
+# Testers
+tester = HypergeometricTest()
+
+
+# Main
 if __name__ == '__main__':
-  termMapper = TermMapping()
-  print(termMapper.getTermMapping())
-  app.run(debug=True)
+    app.run(debug=True)
 
